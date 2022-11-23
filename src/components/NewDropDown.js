634ac130b141851fbf67843ac3e9b3376838aea7
Ricky Kuo <rickykuo925@gmail.com>
2022-11-23 23:20:14 +0000

    remove unused className

--- src/components/NewDropDown.js
+++ src/components/NewDropDown.js
@@ -10,10 +10,7 @@ function NewDropDown() {
                 {MenuItems.map((item, index) => {
                     return (
                         <li className="mb-4 mx-6 py-2 px-4 rounded-xl hover:bg-blue-700 text-center" key={index}>
-                            <Link
-                                className=""
-                                to={item.path}
-                            >
+                            <Link to={item.path}>
                                 {item.title}
                             </Link>
                         </li>
